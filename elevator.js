610a5c8b8b03c143c6dd804b8f769f558e32c6b0
Chris Lamothe <chrislamothe@gmail.com>
2015-01-25 11:56:16 -0700

    Added new event listeners
    Event listeners for floor_button_pressed, up_button_pressed and
    down_button_pressed

--- elevator.js
+++ elevator.js
@@ -1,7 +1,9 @@
 {
     /* 
         My own elevator logic for playing the awesome Elevator Saga
-        http://play.elevatorsaga.com/
+        
+        Site: http://play.elevatorsaga.com/
+        Documentation: http://play.elevatorsaga.com/documentation.html
         
         Author: Chris Lamothe
         License: GNU GPL Version 2
@@ -12,12 +14,35 @@
         //iterate through all elevators
         for (var i = 0; i < elevators.length; i++) {
 
+
+            //idle event listener
             elevators[i].on("idle", function() {
                 
                 //basic loop for going to all floors, from bottom to top
                 for (var j = 0; j < floors.length; j++) {
                     this.goToFloor(j);   
                 }
+            });
+
+            //button press event listener
+            elevators[i].on("floor_button_pressed", function(floorNum) {
+
+                
+            } );
+ 
+        }
+        
+        //iterate through all floors
+        for(var i = 0; i < floors.length; i++) {
+
+            //event listener for press of up button
+            floors[i].on('up_button_pressed', function() {
+
+            });
+
+            //event flister for press of down button
+            floors[i].on('down_button_pressed', function() {
+
             });
         }
     },
